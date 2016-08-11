fc86bee66b213c492cead30f7c0312343d78d389
Andrea Luongo <aluo@win.dtu.dk>
2016-08-11 11:32:03 +0200

    no message

--- DistributionGenerator.m
+++ DistributionGenerator.m
@@ -7,12 +7,17 @@ function [ theta, phi, pdf ] = DistributionGenerator( distributions_number,sampl
         eps2 = rand(distribution_number, samples_number);
         phi = 2 * pi * eps2;
         theta = atan(sqrt(-alpha^2 * log(1-eps1)));
+
     end
     if strcmp(distribution_name, 'ggx')
     end
     if strcmp(distribution_name, 'phong')
     end
+    pdf = {};
+    for i = 1:distribution_numbers
+        pdf{i} = histogram(theta(i,:), 'Normalization','probability');
+        pdf{i}.BinEdges = linspace(0, pi/2,41);
+    end
     
-
 end
 
